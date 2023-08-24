376425490a76cd31d32975a3c6a52bb1f648e980
George Townley <george@knowlesavenue.com>
2023-08-24 16:48:40 +0100

    added additional audio files and trimmed comments

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
-const { Client, IntentsBitField } = require("discord.js");
+const { Client, IntentsBitField, VoiceState } = require("discord.js");
+const {
+  createAudioPlayer,
+  joinVoiceChannel,
+  createAudioResource,
+  VoiceConnectionStatus,
+} = require("@discordjs/voice");
 
 const client = new Client({
   intents: [
@@ -12,11 +18,19 @@ const client = new Client({
 });
 
 client.on("ready", (c) => {
-  console.log("The bot is ready to flame the server.");
+  console.log("The bot is online.");
 });
+
+const audioPlayer = createAudioPlayer();
 
 client.on("voiceStateUpdate", (oldState, newState) => {
   const member = newState.member;
+
+  // Check if the member is a bot
+  if (member.user.bot) {
+    return; // Ignore bot actions
+  }
+
   const oldChannel = oldState.channel;
   const newChannel = newState.channel;
 
@@ -25,13 +39,40 @@ client.on("voiceStateUpdate", (oldState, newState) => {
     const messageContent = `${member.user.tag} joined the voice channel "${newChannel.name}" in ${newChannel.guild.name}.`;
     console.log(messageContent);
 
-    // Send the chat message
-    const textChannel = client.channels.cache.get("699728349861249028");
-    if (textChannel) {
-      textChannel
-        .send(messageContent)
-        .then((message) => console.log("Chat message sent:", message.content))
-        .catch(console.error);
+    // Get the voice channel the user joined
+    const channel = newState.channel;
+    if (channel) {
+      // Join the voice channel
+      const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+
+      // Set the audio player as the connection's subscriber
+      connection.subscribe(audioPlayer);
+
+      const sounds = [
+        "nukeslegal.mp3",
+        "swmg.mp3",
+        "nukingisnowlegal.mp3",
+        "smokedoutnukeparty.mp3",
+        "thisstuffgarbage.mp3",
+        "djsmokeyexclusive.mp3",
+      ];
+      const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+
+      // Load the random sound as the audio resource
+      const resource = createAudioResource(randomSound);
+      audioPlayer.play(resource);
+
+      connection.on(VoiceConnectionStatus.Failed, (error) => {
+        console.error(error);
+      });
+
+      connection.on(VoiceConnectionStatus.Failed, (error) => {
+        console.error(error);
+      });
     }
   }
 
@@ -44,15 +85,49 @@ client.on("voiceStateUpdate", (oldState, newState) => {
     const messageContent = `${member.user.tag} moved from "${oldChannel.name}" to "${newChannel.name}" in ${newChannel.guild.name}.`;
     console.log(messageContent);
 
-    // code to print the message in chat
-    const textChannel = client.channels.cache.get("699728349861249028");
-    if (textChannel) {
-      textChannel
-        .send(messageContent)
-        .then((message) => console.log("Chat message sent:", message.content))
-        .catch(console.error);
+    // Get the voice channel the user joined
+    const channel = newState.channel;
+    if (channel) {
+      // Join the voice channel
+      const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+
+      // Set the audio player as the connection's subscriber
+      connection.subscribe(audioPlayer);
+
+      const sounds = [
+        "nukeslegal.mp3",
+        "swmg.mp3",
+        "nukingisnowlegal.mp3",
+        "smokedoutnukeparty.mp3",
+        "thisstuffgarbage.mp3",
+        "djsmokeyexclusive.mp3",
+      ];
+      const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+
+      // Load the random sound as the audio resource
+      const resource = createAudioResource(randomSound);
+      audioPlayer.play(resource);
+
+      connection.on(VoiceConnectionStatus.Failed, (error) => {
+        console.error(error);
+      });
     }
   }
 });
 
+const { generateDependencyReport } = require("@discordjs/voice");
+console.log(generateDependencyReport());
+
 client.login(process.env.TOKEN);
+
+const restartInterval = 20 * 60 * 1000; // 20 minutes in milliseconds
+
+setInterval(() => {
+  console.log("Restarting the bot...");
+  client.destroy();
+  client.login(process.env.TOKEN);
+}, restartInterval);
